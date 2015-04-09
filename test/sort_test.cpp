46df683077bd86a15cbd6d7e0de384e52e8af041
Daniel Bevenius <daniel.bevenius@gmail.com>
2015-04-09 13:27:49 +0200

    Adding test for shell sort.

--- test/sort_test.cpp
+++ test/sort_test.cpp
@@ -13,6 +13,16 @@ TEST(insertionsort, one) {
   EXPECT_EQ(5, insertion_sort(unsorted, len)[4]);
 }
 
+TEST(shellsort, one) {
+  int unsorted[] = {2, 4, 1, 5, 3};
+  int len = 5;
+  EXPECT_EQ(1, shell_sort(unsorted, len)[0]);
+  EXPECT_EQ(2, shell_sort(unsorted, len)[1]);
+  EXPECT_EQ(3, shell_sort(unsorted, len)[2]);
+  EXPECT_EQ(4, shell_sort(unsorted, len)[3]);
+  EXPECT_EQ(5, shell_sort(unsorted, len)[4]);
+}
+
 TEST(selectionsort, one) {
   int unsorted[] = {2, 4, 1, 5, 3};
   int len = 5;
