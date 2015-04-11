40bb46cf230ab0c66605b75036f24e08612a5d9a
Daniel Bevenius <daniel.bevenius@gmail.com>
2015-04-11 13:19:46 +0200

    Extracting shellsort into separate file to easy debugging.

--- src/shellsort.cpp
+++ src/shellsort.cpp
@@ -1,5 +1,24 @@
 #include <stdio.h>
 #include "sort.hpp"
+int* shell_sort (int arr[], size_t len) {
+    int h = 1;
+    while (h < len / 3) {
+        h = 3 * h + 1;
+    }
+    while (h >= 1) {
+        printf("h=%d\n", h);
+        for (int i = h; i < len; i++) {
+            printf("i=%d\n", i);
+            for (int j = i; j >= h && arr[j] < arr[j-h]; j -= h) {
+                int tmp = arr[j];
+                arr[j] = arr[j-h];
+                arr[j-h] = tmp;
+            }
+        }
+        h /= 3;
+    }
+    return arr;
+}
 
 int main() {
     int unsorted[] = {4, 1, 2, 3};
---
+++ src/shellsort.hpp
@@ -0,0 +1,2 @@
+#include <stddef.h>
+int* shell_sort(int arr[], size_t len);
