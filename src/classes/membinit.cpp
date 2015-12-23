c71c6b873a9cca29e0557d5e4024bf4158f2cf61
Daniel Bevenius <daniel.bevenius@gmail.com>
2015-12-23 13:31:47 +0100

    Adding different variable initialization examples.

--- src/classes/membinit.cpp
+++ src/classes/membinit.cpp
@@ -18,5 +18,18 @@ int main(void) {
   char * n = name;
   MemberInit t = MemberInit(name);
   cout << t.getName() << endl;
+  // value initialization
+  std::string s1 {}; 
+  std::string s2 = std::string{};
+  cout << "s1=" << s1<< endl;
+  cout << "s2=" << s2<< endl;
+  // direct initialization
+  std::string s3 ("s3 value");
+  std::string s4 = std::string("s4 value");
+  cout << "s3=" << s3<< endl;
+  cout << "s4=" << s4<< endl;
+  // copy intitialization 
+  std::string s5 = "s5 value";
+  cout << "s5=" << s5 << endl;
   return 0;
 }
