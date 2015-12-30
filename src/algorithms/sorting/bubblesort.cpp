5dbcde6ecfbe9713be4a6998f546381eba5f8211
Daniel Bevenius <daniel.bevenius@gmail.com>
2015-12-30 06:17:09 +0100

    Removing "inline" comments.

--- src/algorithms/sorting/bubblesort.cpp
+++ src/algorithms/sorting/bubblesort.cpp
@@ -6,10 +6,8 @@
  * When the array is already sorted the complexity is O(n)
  */
 int* bubble_sort (int arr[], size_t len) {
-    // keep track of the passes over the array, starting with one. O(n)
     for (int i = 1; i < len; i++) {
         bool swapped = false;
-        // first pass N-1 comparisions, second pass N-2 comparisions...N*(N-1)/2. 5*(5-1)/2 = 20/2 = 10
         for (int j = 0; j < len - i; j++) {
             int next = j+1;
             if (arr[j] > arr[next]) {
