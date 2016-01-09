320c4f332810c801719c068c3e1cf3f76e2a1178
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-01-09 19:48:46 +0100

    Minor refactoring to insertionsort_test

--- test/insertionsort_test.cpp
+++ test/insertionsort_test.cpp
@@ -5,10 +5,11 @@
 TEST(insertionsort, one) {
   int unsorted[] = {2, 4, 1, 5, 3};
   int len = 5;
-  EXPECT_EQ(1, insertion_sort(unsorted, len)[0]);
-  EXPECT_EQ(2, insertion_sort(unsorted, len)[1]);
-  EXPECT_EQ(3, insertion_sort(unsorted, len)[2]);
-  EXPECT_EQ(4, insertion_sort(unsorted, len)[3]);
-  EXPECT_EQ(5, insertion_sort(unsorted, len)[4]);
+  int *sorted = insertion_sort(unsorted, len);
+  EXPECT_EQ(1, sorted[0]);
+  EXPECT_EQ(2, sorted[1]);
+  EXPECT_EQ(3, sorted[2]);
+  EXPECT_EQ(4, sorted[3]);
+  EXPECT_EQ(5, sorted[4]);
 }
 
