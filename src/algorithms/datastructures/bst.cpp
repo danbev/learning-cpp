b513754b7ccb4f0ddf66803b1649f190ceffc71e
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-01-26 07:25:22 +0100

    Removing inline functions.

--- src/algorithms/datastructures/bst.cpp
+++ src/algorithms/datastructures/bst.cpp
@@ -1,55 +1,65 @@
 #include <iostream>
 #include "bst.hpp"
 
-class BinarySearchTree: SearchTree {
+//class BinarySearchTree: public SearchTree {
+class BinarySearchTree {
   private:
     Node *root;
-    Node * putNode(Node *node, int key, char value) {
-      if (node == NULL) {
-          return new Node(key, value);
-      }
-      if (key < node->key) {
-        node->left = putNode(node->left, key, value);
-      } else if (key > node->key) {
-        node->right = putNode(node->right, key, value);
-      } else {
-        node->value = value;
-      }
-      // we have to increment the size of all nodes, not just the node added.
-      node->count = size(node->left) + size(node->right) + 1;
-      return node;
-    }
-    Node * getNode(Node *node, int key) {
-      if (node == NULL) {
-        return NULL;
-      }
-      if (key < node->key) {
-        return getNode(node->left, key);
-      } else if (key > node->key) {
-        return getNode(node->right, key);
-      } else {
-        return node;
-      }
-    }
-    int size(Node *node) {
-      return node == NULL ? 0 : node->count;
-    }
+    Node * putNode(Node *node, int key, char value);
+    Node * getNode(Node *node, int key);
+    int size(Node *node);
   public:
     BinarySearchTree() : root(NULL) {
     }
     ~BinarySearchTree() {
     }
-    void put(int key, char value) {
-      root = putNode(root, key, value);
-    }
-    char get(int key) {
-      Node *node = getNode(root, key);
-      if (node == NULL) {
-        throw "No such key";
-      }
-      return node->value;
-    }
-    int size() {
-      return size(root);
-    }
+    void put(int key, char value);
+    char get(int key);
+    int size();
 };
+void BinarySearchTree::put(int key, char value) {
+  root = putNode(root, key, value);
+}
+char BinarySearchTree::get(int key) {
+  Node *node = getNode(root, key);
+  if (node == NULL) {
+    throw "No such key";
+  }
+  return node->value;
+}
+
+int BinarySearchTree::size() {
+  return size(root);
+}
+
+Node * BinarySearchTree::putNode(Node *node, int key, char value) {
+  if (node == NULL) {
+      return new Node(key, value);
+  }
+  if (key < node->key) {
+    node->left = putNode(node->left, key, value);
+  } else if (key > node->key) {
+    node->right = putNode(node->right, key, value);
+  } else {
+    node->value = value;
+  }
+  // we have to increment the size of all nodes, not just the node added.
+  node->count = size(node->left) + size(node->right) + 1;
+  return node;
+}
+Node * BinarySearchTree::getNode(Node *node, int key) {
+  if (node == NULL) {
+    return NULL;
+  }
+  if (key < node->key) {
+    return getNode(node->left, key);
+  } else if (key > node->key) {
+    return getNode(node->right, key);
+  } else {
+    return node;
+  }
+}
+int BinarySearchTree::size(Node *node) {
+  return node == NULL ? 0 : node->count;
+}
+
