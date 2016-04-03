efa910e15b2aa84b6b7b23dcbe2d1ab3415f9894
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-04-03 13:08:10 +0200

    Fixing indentation

--- test/algorithms/graphs/depth_first_search_test.cpp
+++ test/algorithms/graphs/depth_first_search_test.cpp
@@ -4,18 +4,18 @@
 #include "depth-first-search.cpp"
 
 TEST(DepthFirstSearch, search) {
-  UndirectedGraph graph {20};
-  graph.addEdge(0, 1);
-  graph.addEdge(0, 2);
-  graph.addEdge(0, 5);
-  graph.addEdge(1, 7);
-  graph.addEdge(8, 9);
-  DepthFirstSearch search (&graph, 0);
-  EXPECT_EQ(true, search.isConnectedToSource(1));
-  EXPECT_EQ(true, search.isConnectedToSource(2));
-  EXPECT_EQ(true, search.isConnectedToSource(5));
-  EXPECT_EQ(true, search.isConnectedToSource(7));
-  EXPECT_EQ(false, search.isConnectedToSource(8));
-  EXPECT_EQ(false, search.isConnectedToSource(9));
+    UndirectedGraph graph {20};
+    graph.addEdge(0, 1);
+    graph.addEdge(0, 2);
+    graph.addEdge(0, 5);
+    graph.addEdge(1, 7);
+    graph.addEdge(8, 9);
+    DepthFirstSearch search (&graph, 0);
+    EXPECT_EQ(true, search.isConnectedToSource(1));
+    EXPECT_EQ(true, search.isConnectedToSource(2));
+    EXPECT_EQ(true, search.isConnectedToSource(5));
+    EXPECT_EQ(true, search.isConnectedToSource(7));
+    EXPECT_EQ(false, search.isConnectedToSource(8));
+    EXPECT_EQ(false, search.isConnectedToSource(9));
 }
 
--- test/algorithms/graphs/search_test.cpp
+++ test/algorithms/graphs/search_test.cpp
@@ -4,15 +4,15 @@
 #include "search.cpp"
 
 TEST(Graph, search) {
-  UndirectedGraph graph {20};
-  graph.addEdge(0, 1);
-  graph.addEdge(0, 2);
-  graph.addEdge(0, 5);
-  graph.addEdge(1, 7);
-  Search search (&graph, 0);
-  EXPECT_EQ(true, search.isConnectedToSource(1));
-  EXPECT_EQ(true, search.isConnectedToSource(2));
-  EXPECT_EQ(true, search.isConnectedToSource(5));
-  EXPECT_EQ(false, search.isConnectedToSource(7));
+    UndirectedGraph graph {20};
+    graph.addEdge(0, 1);
+    graph.addEdge(0, 2);
+    graph.addEdge(0, 5);
+    graph.addEdge(1, 7);
+    Search search (&graph, 0);
+    EXPECT_EQ(true, search.isConnectedToSource(1));
+    EXPECT_EQ(true, search.isConnectedToSource(2));
+    EXPECT_EQ(true, search.isConnectedToSource(5));
+    EXPECT_EQ(false, search.isConnectedToSource(7));
 }
 
