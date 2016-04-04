5ab95f2bfaf11f70fddc9c8b18879e876264f855
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-04-04 09:40:19 +0200

    Adding an example of using std::unique_ptr

--- test/algorithms/graphs/search_test.cpp
+++ test/algorithms/graphs/search_test.cpp
@@ -4,12 +4,12 @@
 #include "search.cpp"
 
 TEST(Graph, search) {
-    UndirectedGraph graph {20};
-    graph.addEdge(0, 1);
-    graph.addEdge(0, 2);
-    graph.addEdge(0, 5);
-    graph.addEdge(1, 7);
-    Search search (&graph, 0);
+    std::unique_ptr<UndirectedGraph> graph(new UndirectedGraph(20));
+    graph->addEdge(0, 1);
+    graph->addEdge(0, 2);
+    graph->addEdge(0, 5);
+    graph->addEdge(1, 7);
+    Search search (graph.get(), 0);
     EXPECT_EQ(true, search.isConnectedToSource(1));
     EXPECT_EQ(true, search.isConnectedToSource(2));
     EXPECT_EQ(true, search.isConnectedToSource(5));
