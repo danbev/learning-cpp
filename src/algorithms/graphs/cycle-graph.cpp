84300ef6ab058eb4673ab99f1b0d2c16cd112326
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-04-16 09:33:36 +0200

    Adding CycleGraph example.
    Motivation:
    Example that determines whether a graph is acyclic or not.

---
+++ src/algorithms/graphs/cycle-graph.cpp
@@ -0,0 +1,54 @@
+#include "graph.h"
+#include <iostream>
+#include "../datastructures/stack.cpp"
+
+/*
+ * Used depth first to search the Graph and detect a cycle in the graph.
+ *
+ */
+class CycleGraph {
+    private:
+        const Graph* graph;
+        bool* visited;
+        void dfs(const Graph* g, int v, int u);
+        bool cycle;
+    public:
+        CycleGraph(const Graph * graph);
+        bool hasCycle() const;
+};
+
+CycleGraph::CycleGraph(const Graph *graph) {
+    this->graph = graph;
+    int length = this->graph->vertices();
+    visited = new bool[length] {false};
+    for (int v = 0; v < length; v++) {
+        if (!visited[v]) {
+            dfs(this->graph, v, v);
+        }
+    }
+}
+
+void CycleGraph::dfs(const Graph *g, int v, int u) {
+    visited[v] = true; 
+    const IntIterator *it = g->adjacent(v);
+    while(it->hasNext()) {
+        int w = it->next();
+        if (!visited[w]) {
+            dfs(g, w, v);
+        } 
+        // if vertex w has already been visited/marked but is the same as the "parent" 
+        // or the vertex that we came from, then this does not indicate a cycle as that 
+        // is simply a connection in the graph.
+        // But lets say that another adjacent vertex w, which remember has already been
+        // visited/marked, but is not equal to the parent this indicates that we have 
+        // circled around. If w had not been visisted we could have recursed into dfs
+        // again.
+        else if (w != u) { 
+            cycle = true;
+        }
+    }
+}
+
+bool CycleGraph::hasCycle() const {
+    return cycle;
+}
---
+++ test/algorithms/graphs/cycle_graph_test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <gtest/gtest.h>
+#include "undirected-graph.cpp"
+#include "cycle-graph.cpp"
+
+TEST(CycleGraph, cyclic) {
+    UndirectedGraph graph {5};
+    //     (0)-------(1)
+    //      |         |
+    //     (2)-------(3)
+    graph.addEdge(0, 1);
+    graph.addEdge(0, 2);
+    graph.addEdge(2, 3);
+    graph.addEdge(1, 3);
+    const CycleGraph cycle (&graph);
+    EXPECT_EQ(true, cycle.hasCycle());
+}
+
+TEST(CycleGraph, acyclic) {
+    UndirectedGraph graph {4};
+    //     (0)       (1)
+    //      |         |
+    //     (2)-------(3)
+    graph.addEdge(0, 2);
+    graph.addEdge(2, 3);
+    graph.addEdge(1, 3);
+    const CycleGraph cycle (&graph);
+    EXPECT_EQ(false, cycle.hasCycle());
+}
