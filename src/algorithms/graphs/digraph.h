1d941a281bcbea7e22908bf4438a5d444b91610f
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-04-24 13:11:32 +0200

    Adding some digraph documentation.

--- src/algorithms/graphs/digraph.h
+++ src/algorithms/graphs/digraph.h
@@ -1,6 +1,18 @@
 #include "../datastructures/int-iterator.h"
 #ifndef DIGRAPH
 #define DIGRAPH
+/*
+ * A directed graph where edges are one way paths between vertices.
+ *
+ * Terminology:
+ * Edge: a one way path between two vertices.
+ * Directed path: in a graph is a sequence of vertices where there is an edge pointing from each
+ *                vertex to its successor (comes after).
+ * Simple path: is a path with no repeated vertices.
+ * Directed cycle: is a directed path with at least one edge whose first and last vertices are the same.
+ * Length: the number of edges in the path/cycle.
+ *
+ */
 class Digraph {
     public:
         virtual int vertices() const = 0;
