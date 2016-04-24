a6276830b3a9ad6315209a29db82ae853ad3b698
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-04-24 10:09:44 +0200

    Changing class name to Digraph

--- src/algorithms/graphs/digraph.h
+++ src/algorithms/graphs/digraph.h
@@ -1,12 +1,12 @@
 #include "../datastructures/int-iterator.h"
 #ifndef DIGRAPH
 #define DIGRAPH
-class DiGraph {
+class Digraph {
     public:
         virtual int vertices() const = 0;
         virtual int edges() const = 0;
         virtual void addEdge(int v, int w) = 0;
         virtual const IntIterator* adjacent(int v) const = 0;
-        virtual const DiGraph* reverse() const = 0;
+        virtual const Digraph* reverse() const = 0;
 };
 #endif
