31a74f2e5fb208bcfd79876667ff2baed2c8f23f
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-04-25 19:40:38 +0200

    Adding directed breadth first path search implementation.

---
+++ src/algorithms/graphs/directed-breadth-first-path-search.cpp
@@ -0,0 +1,71 @@
+#include "graph.h"
+#include "../datastructures/stack.cpp"
+#include "../datastructures/queue.cpp"
+
+class DirectedBreadthFirstPathSearch {
+    private:
+        const Digraph* graph;
+        int s;
+        bool* visited;
+        int *parentVertex;
+        int count;
+        void bfs(const Digraph* g, int v);
+    public:
+        DirectedBreadthFirstPathSearch(const Digraph * graph, int s);
+        bool isConnectedToSource(int v) const;
+        int visitedVertices() const;
+        const IntIterator* pathTo(int v) const;
+};
+
+DirectedBreadthFirstPathSearch::DirectedBreadthFirstPathSearch(const Digraph *graph, int s) {
+    this->graph = graph;
+    this->s = s;
+    this->count = 0;
+    int length = this->graph->vertices();
+    visited = new bool[length] {false};
+    parentVertex = new int[length];
+    for (int i = 0; i < length; i++) {
+        parentVertex[i] = -1;
+    }
+    bfs(this->graph, s);
+}
+
+void DirectedBreadthFirstPathSearch::bfs(const Digraph *g, int s) {
+    Queue queue;
+    visited[s] = true; // mark the source
+    queue.enqueue(s);
+    while (!queue.empty()) {
+        count++;
+        int v = queue.dequeue();
+        const IntIterator *it = g->adjacent(v);
+        while (it->hasNext()) {
+            int w = it->next();
+            if (!visited[w]) {
+                parentVertex[w] = v;
+                visited[w] = true;
+                queue.enqueue(w);
+            }
+        }
+    }
+}
+
+auto DirectedBreadthFirstPathSearch::pathTo(int v) const -> const IntIterator * {
+    if (!isConnectedToSource(v)) {
+        // TODO: return an empty IntIterator
+        return nullptr;
+    }
+    Stack stack;
+    for (int i = v; i != s; i = parentVertex[i]) {
+        stack.push(i);
+    }
+    stack.push(s);
+    return stack.iterator();
+}
+
+auto DirectedBreadthFirstPathSearch::isConnectedToSource(int v) const -> bool {
+    return visited[v];
+}
+
+auto DirectedBreadthFirstPathSearch::visitedVertices() const -> int {
+    return count;
+}
---
+++ test/algorithms/graphs/directed_breadth_first_path_search_test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <gtest/gtest.h>
+#include "directed-graph.cpp"
+#include "directed-breadth-first-path-search.cpp"
+
+/*
+ *   (0)________>(2)
+ *    |         /   \
+ *    |        /     \
+ *   (5)----(3)-----(4)
+ */
+TEST(DirectedBreadthFirstPathSearch, pathTo) {
+    DirectedGraph graph {20};
+    graph.addEdge(0, 2);
+    graph.addEdge(2, 3);
+    graph.addEdge(2, 4);
+    graph.addEdge(3, 5);
+    graph.addEdge(0, 5);
+    const DirectedBreadthFirstPathSearch search (&graph, 0);
+    const IntIterator* it = search.pathTo(5);
+    EXPECT_EQ(0, it->next());
+    EXPECT_EQ(5, it->next());
+    EXPECT_EQ(false, it->hasNext());
+}
+
