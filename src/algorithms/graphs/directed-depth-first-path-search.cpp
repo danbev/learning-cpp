866966a4684b0c7611a5eb7b6c61af0e82e2c427
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-04-25 19:25:26 +0200

    Adding directed depth first path search implementation.
    Motivation:
    This is the same code as for an undirected depth first path search and
    it to make the point that they are identical.

---
+++ src/algorithms/graphs/directed-depth-first-path-search.cpp
@@ -0,0 +1,64 @@
+#include "graph.h"
+#include "../datastructures/stack.cpp"
+
+class DirectedDepthFirstPathSearch {
+    private:
+        const Digraph* graph;
+        int s;
+        bool* visited;
+        int *parentVertex;
+        int count;
+        void dfs(const Digraph* g, int v);
+    public:
+        DirectedDepthFirstPathSearch(const Digraph * graph, int s);
+        bool isConnectedToSource(int v) const;
+        int visitedVertices() const;
+        const IntIterator* pathTo(int v) const;
+};
+
+DirectedDepthFirstPathSearch::DirectedDepthFirstPathSearch(const Digraph *graph, int s) {
+    this->graph = graph;
+    this->s = s;
+    this->count = 1;
+    int length = this->graph->vertices();
+    visited = new bool[length] {false};
+    parentVertex = new int[length];
+    for (int i = 0; i < length; i++) {
+        parentVertex[i] = -1;
+    }
+    dfs(this->graph, s);
+}
+
+void DirectedDepthFirstPathSearch::dfs(const Digraph *g, int v) {
+    visited[v] = true; // first step is to mark the vertex as visited
+    count++;
+    const IntIterator *it = g->adjacent(v);
+    while(it->hasNext()) {
+        int w = it->next();
+        if (!visited[w]) {
+            parentVertex[w] = v;
+            dfs(g, w);
+        }
+    }
+}
+
+auto DirectedDepthFirstPathSearch::pathTo(int v) const -> const IntIterator * {
+    if (!isConnectedToSource(v)) {
+        // TODO: return an empty IntIterator
+        return nullptr;
+    }
+    Stack stack;
+    for (int i = v; i != s; i = parentVertex[i]) {
+        stack.push(i);
+    }
+    stack.push(s);
+    return stack.iterator();
+}
+
+auto DirectedDepthFirstPathSearch::isConnectedToSource(int v) const -> bool {
+    return visited[v];
+}
+
+auto DirectedDepthFirstPathSearch::visitedVertices() const -> int {
+    return count;
+}
---
+++ test/algorithms/graphs/directed_depth_first_path_search_test.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <gtest/gtest.h>
+#include "directed-graph.cpp"
+#include "directed-depth-first-path-search.cpp"
+
+/*
+ * (Edges are directed)
+ *   (0)________>(2)
+ *              /   \
+ *            \/    \/
+ *   (5)<---(3)<----(4)
+ */
+TEST(DirectedDepthFirstPathSearch, pathTo) {
+    DirectedGraph graph {10};
+    graph.addEdge(0, 2);
+    graph.addEdge(2, 4);
+    graph.addEdge(2, 3);
+    graph.addEdge(3, 5);
+
+    const DirectedDepthFirstPathSearch search (&graph, 0);
+    const IntIterator* it = search.pathTo(5);
+    EXPECT_EQ(0, it->next());
+    EXPECT_EQ(2, it->next());
+    EXPECT_EQ(3, it->next());
+    EXPECT_EQ(5, it->next());
+    EXPECT_EQ(false, it->hasNext());
+}
+
