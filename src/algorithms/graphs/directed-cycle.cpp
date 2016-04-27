e4ae807bb18116b9c2daba13055a751d792ad5f2
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-04-27 19:18:33 +0200

    Adding a directed cycle "checker" implementation.
    Motivation:
    The DirectedCycle class can be used to determine if a digraph has a
    cycle, and if so return the cycle.

---
+++ src/algorithms/graphs/directed-cycle.cpp
@@ -0,0 +1,82 @@
+#include "graph.h"
+#include "../datastructures/stack.cpp"
+
+/**
+ * Determines is a digraph has a cycle and if so return the vertices
+ * of that cycle.
+ *
+ * Some use case do not allow for a cycle to be present. For example if
+ * you have a number of jobs a, b, and c. And a must be done before b,
+ * and b before c, and c before a we have:
+ *     (a)----->(b)
+ *     / \       |
+ *        \      |
+ *          \    \/
+ *            - (c)
+ * This can be used to answer whether a graph is a Directed Acyclic Graph (DAG).
+ *
+ * For a use case of a DAG you can think of how git works. It has vertexes that are
+ * commits and edges from a child to one or more parents.
+ */
+class DirectedCycle {
+    private:
+        const Digraph* graph;
+        bool* visited;
+        Stack* stack;
+        bool* onstack;
+        int *parentVertex;
+        void dfs(const Digraph* g, int v);
+    public:
+        DirectedCycle(const Digraph * graph);
+        const IntIterator* cycle() const;
+        bool hasCycle() const;
+};
+
+DirectedCycle::DirectedCycle(const Digraph *graph) {
+    this->graph = graph;
+    this->stack = nullptr;
+    int length = this->graph->vertices();
+    visited = new bool[length] {false};
+    onstack = new bool[length] {false};
+    parentVertex = new int[length];
+    for (int i = 0; i < length; i++) {
+        parentVertex[i] = -1;
+    }
+    dfs(this->graph, 0);
+}
+
+void DirectedCycle::dfs(const Digraph *g, int v) {
+    // keep track of the vertex currently being processed.
+    onstack[v] = true;
+    visited[v] = true;
+    const IntIterator *it = g->adjacent(v);
+    while(it->hasNext()) {
+        // check if the last recursion found a cycle
+        if (hasCycle()) {
+            return;
+        }
+        int w = it->next();
+        if (!visited[w]) {
+            parentVertex[w] = v;
+            dfs(g, w);
+        } else if (onstack[w]) {
+            // if vertex w is already on the stack it means that we have already visited
+            // this vertex when recursively processing vertex v so we have a cycle.
+            stack = new Stack();
+            for (int i = v; i != w; i = parentVertex[i]) {
+                stack->push(i);
+            }
+            stack->push(w);
+            stack->push(v);
+        }
+    }
+    onstack[v] = false;
+}
+
+auto DirectedCycle::cycle() const -> const IntIterator * {
+    return stack->iterator();
+}
+
+auto DirectedCycle::hasCycle() const -> bool {
+    return stack != nullptr;
+}
---
+++ test/algorithms/graphs/directed_cycle_test.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <gtest/gtest.h>
+#include "directed-graph.cpp"
+#include "directed-cycle.cpp"
+
+TEST(DirectedCycle, acyclic) {
+    DirectedGraph graph {10};
+    graph.addEdge(0, 2);
+    graph.addEdge(2, 4);
+    graph.addEdge(2, 3);
+    graph.addEdge(3, 5);
+    const DirectedCycle cycle (&graph);
+    EXPECT_EQ(false, cycle.hasCycle());
+}
+
+/*
+ * (0)--------->(1)
+ *  /\           |
+ *  |            |
+ * (3)<---------(2)
+ */
+TEST(DirectedCycle, cyclic) {
+    DirectedGraph graph {6};
+    graph.addEdge(0, 1);
+    graph.addEdge(1, 2);
+    graph.addEdge(2, 3);
+    graph.addEdge(3, 0);
+    const DirectedCycle cycle (&graph);
+    EXPECT_EQ(true, cycle.hasCycle());
+    const IntIterator* it = cycle.cycle();
+    EXPECT_EQ(3, it->next());
+    EXPECT_EQ(0, it->next());
+    EXPECT_EQ(1, it->next());
+    EXPECT_EQ(2, it->next());
+}
+
