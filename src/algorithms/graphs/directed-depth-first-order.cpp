3fe7dd831714e312b6e84077fb09ee1e5fd56edb
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-04-28 19:19:21 +0200

    Adding digraph depth first order processor impl.

---
+++ src/algorithms/graphs/directed-depth-first-order.cpp
@@ -0,0 +1,74 @@
+#include "graph.h"
+#include "../datastructures/stack.cpp"
+#include "../datastructures/queue.cpp"
+
+namespace graphs {
+
+    /**
+     * Performs a depth first search on a digraph and stores the vertext 
+     * visited pre and post processing.
+     *
+     * In addition to storing the post processing vertexes the reverse of 
+     * this order is also made available to clients.
+     *
+     * These orders are useful in advanced graph-processing algoritms.
+     *
+     */
+    class DirectedDepthFirstOrder {
+        private:
+            const Digraph* graph;
+            Queue *pre;
+            Queue *post;
+            Stack *reverse;
+            bool* visited;
+            int *parentVertex;
+            void dfs(const Digraph* g, int v);
+        public:
+            DirectedDepthFirstOrder(const Digraph * graph);
+            const IntIterator* preOrder() const;
+            const IntIterator* postOrder() const;
+            const IntIterator* reverseOrder() const;
+    };
+
+    DirectedDepthFirstOrder::DirectedDepthFirstOrder(const Digraph *graph) {
+        this->graph = graph;
+        this->pre = new Queue();
+        this->post = new Queue();
+        this->reverse = new Stack();
+        int length = this->graph->vertices();
+        visited = new bool[length] {false};
+        parentVertex = new int[length];
+        for (int i = 0; i < length; i++) {
+            parentVertex[i] = -1;
+        }
+        dfs(this->graph, 0);
+    }
+
+    void DirectedDepthFirstOrder::dfs(const Digraph *g, int v) {
+        pre->enqueue(v);
+        visited[v] = true; // first step is to mark the vertex as visited
+        const IntIterator *it = g->adjacent(v);
+        while(it->hasNext()) {
+            int w = it->next();
+            if (!visited[w]) {
+                parentVertex[w] = v;
+                dfs(g, w);
+            }
+        }
+        post->enqueue(v);
+        reverse->push(v);
+    }
+
+    auto DirectedDepthFirstOrder::preOrder() const -> const IntIterator * {
+        return pre->iterator();
+    }
+
+    auto DirectedDepthFirstOrder::postOrder() const -> const IntIterator * {
+        return post->iterator();
+    }
+
+    auto DirectedDepthFirstOrder::reverseOrder() const -> const IntIterator * {
+        return reverse->iterator();
+    }
+
+}
---
+++ test/algorithms/graphs/directed_depth_first_order_test.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <gtest/gtest.h>
+#include "directed-graph.cpp"
+#include "directed-depth-first-order.cpp"
+
+TEST(DirectedDepthFirstOrder, order) {
+    DirectedGraph graph {10};
+    graph.addEdge(0, 1);
+    graph.addEdge(1, 2);
+    graph.addEdge(2, 3);
+    const graphs::DirectedDepthFirstOrder t (&graph);
+
+    const IntIterator *pre = t.preOrder();
+    EXPECT_EQ(0, pre->next());
+    EXPECT_EQ(1, pre->next());
+    EXPECT_EQ(2, pre->next());
+    EXPECT_EQ(3, pre->next());
+    EXPECT_EQ(false, pre->hasNext());
+
+    const IntIterator *post = t.postOrder();
+    EXPECT_EQ(3, post->next());
+    EXPECT_EQ(2, post->next());
+    EXPECT_EQ(1, post->next());
+    EXPECT_EQ(0, post->next());
+    EXPECT_EQ(false, post->hasNext());
+
+    const IntIterator *rev = t.reverseOrder();
+    EXPECT_EQ(0, rev->next());
+    EXPECT_EQ(1, rev->next());
+    EXPECT_EQ(2, rev->next());
+    EXPECT_EQ(3, rev->next());
+    EXPECT_EQ(false, rev->hasNext());
+}
+
