9bca36b34c6623ab5ce961cb03a26d208b676a65
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-05-06 06:11:34 +0200

    Adding constructor/descructor to see copying for callable.

--- src/concurrency/basic.cpp
+++ src/concurrency/basic.cpp
@@ -7,20 +7,38 @@ void hello() {
 
 class Task {
     public:
-        void doit() const {
-            std::cout << "Task doing it..." << std::endl;
+        Task(const char* name) {
+            std::cout << "Creating Task " << name << std::endl;
+            this->name_ = name;
+        }
+        Task(const Task& task) {
+            std::cout << "Copying Task " << task.name_ << std::endl;
+            this->name_ = task.name_;
+        }
+        ~Task() {
+            std::cout << "Destroying Task " << name_ << std::endl;
+        }
+        void hello() const {
+            std::cout << name_ << " helo" << std::endl;
         }
         void operator()() const {
-            doit();
+            hello();
         }
+    private:
+        const char* name_;
 };
 
 int main(int argc, char **argv) {
     std::thread t1 {hello};
     t1.join();
 
-    Task task;
-    std::thread t2 {task};
+    // This will first create an instance of type Task using the constructor Task(const char* name)
+    // Then it will create an instance of type Task using the constructor Task(const Task& task)
+    // Then it will create an instance of type Task using the constructor Task(const Task& task) which
+    // will be passed to the new thread.
+    // It will destroy the instance created using the constructor.
+    // It will destroy the instance created using the copy constructor (not the one passed to the thread).
+    std::thread t2 (Task("task 1"));
     t2.join();
     return 0;
 }
