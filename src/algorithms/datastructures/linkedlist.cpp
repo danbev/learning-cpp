4bd3708bb58c8005c73735c9382af08433e1844a
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-05-07 18:10:35 +0200

    Extracting out inline methods from LinkedList.

--- src/algorithms/datastructures/linkedlist.cpp
+++ src/algorithms/datastructures/linkedlist.cpp
@@ -11,10 +11,7 @@ class IntNode {
         int value;
         IntNode *next;
     public:
-        IntNode(int value, IntNode * next) {
-            this->value = value;
-            this->next = next;
-        }
+        IntNode(int value, IntNode * next) : value(value), next(next) {}
         ~IntNode() {}
 };
 
@@ -25,85 +22,94 @@ class LinkedList {
         int length;
     public:
         LinkedList() : head(nullptr), length(0) {}
+        bool empty() const;
+        LinkedList &addFront(int value);
+        int removeFront();
+        int front() const;
+        int size() const;
+        bool contains(int nr) const;
+        const LinkedList &print() const;
+        template<class Func>
+        void foreach(Func &f) const;
+        IntIterator* iterator();
+};
 
-    bool empty() const {
-        return head == nullptr;
+bool LinkedList::empty() const {
+    return head == nullptr;
+}
+
+
+LinkedList& LinkedList::addFront(int value) {
+    IntNode *newNode = new IntNode(value, nullptr);
+    if (head == nullptr) {
+        head = newNode;
+    } else {
+        IntNode *current = head;
+        newNode->next = current;
+        head = newNode;
     }
+    length++;
+    return *this;
+}
 
-    LinkedList &addFront(int value) {
-        IntNode *newNode = new IntNode(value, nullptr);
-        if (head == nullptr) {
-            head = newNode;
-        } else {
-            IntNode *current = head;
-            newNode->next = current;
-            head = newNode;
-        }
-        length++;
-        return *this;
+int LinkedList::removeFront() {
+    if (head == nullptr) {
+        return -1;
     }
+    IntNode *old = head;
+    head = old->next;
+    int value = old->value;
+    delete old;
+    length--;
+    return value;
+}
 
-    int removeFront() {
-        if (head == nullptr) {
-            return -1;
-        }
-        IntNode *old = head;
-        head = old->next;
-        int value = old->value;
-        delete old;
-        length--;
-        return value;
+int LinkedList::front() const {
+    if (head == nullptr) {
+        return -1;
     }
+    return head->next->value;
+}
 
-    int front() const {
-        if (head == nullptr) {
-            return -1;
-        }
-        return head->next->value;
-    }
+int LinkedList::size() const {
+    return length;
+}
 
-    int size() const {
-        return length;
-    }
-
-    bool contains(int nr) const {
-        if (head == nullptr) {
-            return false;
-        }
-        for (IntNode *p = head; p != nullptr; p = p->next) {
-            if (p->value == nr) {
-                return true;
-            }
-        }
+bool LinkedList::contains(int nr) const {
+    if (head == nullptr) {
         return false;
     }
-
-    const LinkedList &print() const {
-        if (head == nullptr) {
-            std::cout << "[empty]" << std::endl;
-        } else {
-            std::cout << "[";
-            for (IntNode * node = head; node != nullptr; node = node->next) {
-                std::cout << ", " << node->value;
-            }
-            std::cout << "]" << std::endl;
-        }
-        return *this;
-    }
-
-    template<class Func>
-    void foreach(Func &f) {
-        if (empty()) {
-            return;
-        }
-        for (IntNode* node = head; node != nullptr; node = node->next) {
-            f(node->value);
+    for (IntNode *p = head; p != nullptr; p = p->next) {
+        if (p->value == nr) {
+            return true;
         }
     }
+    return false;
+}
 
-    IntIterator* iterator();
+const LinkedList& LinkedList::print() const {
+    if (head == nullptr) {
+        std::cout << "[empty]" << std::endl;
+    } else {
+        std::cout << "[";
+        for (IntNode * node = head; node != nullptr; node = node->next) {
+            std::cout << ", " << node->value;
+        }
+        std::cout << "]" << std::endl;
+    }
+    return *this;
+}
 
-};
+template<class Func>
+void LinkedList::foreach(Func &f) const {
+    if (empty()) {
+        return;
+    }
+    for (IntNode* node = head; node != nullptr; node = node->next) {
+        f(node->value);
+    }
+}
+
 
 class LinkedListIntIterator : public IntIterator {
     private:
