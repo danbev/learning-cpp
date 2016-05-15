ecc2ff3af877c02a73ff10fd318f8fe9a3e949ed
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-05-15 15:27:17 +0200

    Removing int-iterator.h

--- src/algorithms/datastructures/int-iterator.h
+++
@@ -1,9 +0,0 @@
-#include "iterator.h"
-#ifndef INT_ITERATOR
-#define INT_ITERATOR
-class IntIterator: public Iterator<int> {
-    public:
-        virtual int next() = 0;
-        virtual bool hasNext() const = 0;
-};
-#endif
