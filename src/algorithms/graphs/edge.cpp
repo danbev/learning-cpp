8ae121ec0af141798824d4b03bc384d256db9fc6
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-05-17 19:53:28 +0200

    Moving the class declaration of Edge to edge.h.

--- src/algorithms/graphs/edge.cpp
+++ src/algorithms/graphs/edge.cpp
@@ -1,16 +1,4 @@
-
-class Edge {
-    private:
-        int v_;
-        int w_;
-        double weigth_;
-    public:
-        Edge(int v, int w, double weigth) : v_(v), w_(w), weigth_(weigth) {};
-        double weigth() const;
-        int either() const;
-        int other(int vertex) const;
-        int compare(const Edge that) const;
-};
+#include "edge.h"
 
 int Edge::either() const {
     return v_;
--- src/algorithms/graphs/edge.h
+++ src/algorithms/graphs/edge.h
@@ -1,9 +1,15 @@
 #ifndef EDGE_H
 #define EDGE_H
 class Edge {
+    private:
+        int v_;
+        int w_;
+        double weigth_;
     public:
-        double weight();
-        int either();
-        int other();
-}
+        Edge(int v, int w, double weigth) : v_(v), w_(w), weigth_(weigth) {};
+        double weigth() const;
+        int either() const;
+        int other(int vertex) const;
+        int compare(const Edge that) const;
+};
 #endif
