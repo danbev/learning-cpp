2b96874ed18c0ae127c6f6dc47d58cf637da849a
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-05-30 19:41:16 +0200

    Adding unique_ptr example.

--- src/pointers/smart-pointers.cpp
+++ src/pointers/smart-pointers.cpp
@@ -4,11 +4,18 @@
 /**
  * Allows for the destructor for an object to be automatically called
  * when the pointers scope is destroyed.
+ * If a auto_ptr is assigned to another pointer the first one will use ownership
+ * and be invalid.
  */
 std::auto_ptr<std::string> autoPointer() {
     return std::auto_ptr<std::string>(new std::string("bajja"));
 }
 
+/**
+ * Like auto_ptr also has the concept of ownership, but instead of crashing
+ * there will be a compile time error if you try to assign a pointer of this
+ * type to another pointer.
+ */
 std::unique_ptr<std::string> uniquePointer() {
     return std::unique_ptr<std::string>(new std::string("bajja"));
 }
--- test/pointers/smart_pointers_test.cpp
+++ test/pointers/smart_pointers_test.cpp
@@ -12,3 +12,11 @@ TEST(SmartPointers, autoPointer) {
     EXPECT_EQ("bajja", *str2);
     EXPECT_EQ(nullptr, str.get()) << " str one has given up ownership";
 }
+
+TEST(SmartPointers, uniquePointer) {
+    std::unique_ptr<std::string> str = uniquePointer();
+    std::unique_ptr<std::string> str2;
+    EXPECT_EQ("bajja", *str) << "unique_ptr should be able to be dereferenced like a normal pointer";
+    str2 = std::move(str);
+    EXPECT_EQ("bajja", *str2) << "unique_ptr should be movable to another pointer with out a compile time error";
+}
