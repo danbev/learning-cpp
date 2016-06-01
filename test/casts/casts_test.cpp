e71a44fc58f37c828d2f4389af1ccaf545082043
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-06-01 06:58:07 +0200

    Adding a typeid example.

--- test/casts/casts_test.cpp
+++ test/casts/casts_test.cpp
@@ -1,6 +1,9 @@
 #include <gtest/gtest.h>
 #include <iostream>
+#include <typeinfo>
 #include "casts.cpp"
+
+using namespace std;
 
 TEST(Casts, cast) {
     Base* b = cast();
@@ -17,4 +20,10 @@ TEST(Casts, dynamic_cast_to_derived_class) {
     Base* b = new Base();
     Derived* d = dynamic_cast<Derived*>(b);
     EXPECT_EQ(nullptr, d) << "dynamic_cast from base to derived should not succeed. ";
+}
+
+TEST(Casts, typeinfo) {
+    Base* b = new Base();
+    EXPECT_NE(nullptr, typeid(*b).name());
+    EXPECT_EQ(typeid(Base), typeid(*b)) << "both should be of the same type";
 }
