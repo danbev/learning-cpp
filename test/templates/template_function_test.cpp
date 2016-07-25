8bb196c781edec4412772924a0f409a14367be29
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-07-25 09:02:31 +0200

    Adding an example of leaving out template parameters.

--- test/templates/template_function_test.cpp
+++ test/templates/template_function_test.cpp
@@ -8,8 +8,12 @@ using namespace std;
 TEST(TemplateFunction, function) {
   Testing t;
   void (*printit)(int) = Testing::doit<int>;
+  // the template parameters can be explicit
   t.doit<int>(10);
+  // the template parameters can be empty
   t.doit<>("bajja");
+  // the template parameters or left out
+  t.doit("bajja");
   printit(22);
   //EXPECT_EQ(3, Base::destroyed()) << "Destructor should have been called three times";
 }
