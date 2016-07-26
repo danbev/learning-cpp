a3a4087eb71342dd80cd609e4f9bf6ca9e40030c
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-07-26 09:02:09 +0200

    Adding a virtual destructor example.

---
+++ src/fundamentals/constructors/virtual-destructor.cc
@@ -0,0 +1,63 @@
+#include <string>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+class Base {
+  private:
+    static int instances_;
+    static int destroyed_;
+    string name_;
+  public:
+    Base(string name) : name_(name) {
+      cout << "Base constructor called" << endl;
+      instances_++;
+    }
+
+    virtual ~Base() {
+      cout << "Base descructor called " << endl;
+      destroyed_++;
+    }
+
+    string name() const {
+      return name_;
+    }
+
+    static int instances() {
+      return instances_;
+    }
+
+    static int destroyed() {
+      return destroyed_;
+    }
+
+};
+int Base::instances_ = 0;
+int Base::destroyed_ = 0;
+
+class Derived : public Base {
+  private:
+    static int instances_;
+    static int destroyed_;
+  public: 
+    Derived(string name) : Base(name) {
+      cout << "Derived constructor called" << endl;
+      instances_++;
+    }
+    ~Derived() {
+      cout << "Derived descructor called" << endl;
+      destroyed_++;
+    }
+    static int instances() {
+      return instances_;
+    }
+
+    static int destroyed() {
+      return destroyed_;
+    }
+};
+int Derived::instances_ = 0;
+int Derived::destroyed_ = 0;
+
