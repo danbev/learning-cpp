ce150445f05ae3aa00dfb8c3b861d53627cd10e0
Daniel Bevenius <daniel.bevenius@gmail.com>
2019-08-23 05:26:18 +0200

    src: update unique_ptr example

--- src/fundamentals/unique_ptr.cc
+++ src/fundamentals/unique_ptr.cc
@@ -3,15 +3,28 @@
 #include <algorithm>
 
 struct Foo {
-  Foo() { std::cout << "Foo\n"; }
-  ~Foo() { std::cout << "~Foo\n"; }
-  void doit() {
-    std::cout << "doing it..." << '\n';
+  Foo() { std::cout << "Foo ctor\n"; }
+  ~Foo() { 
+    std::cout << "~Foo\n"; 
+    nr = 0;
   }
+  void doit() {
+    std::cout << "doing it..." <<  nr << '\n';
+  }
+  int nr = 10;
 };
 
 
+Foo* something() {
+  std::unique_ptr<Foo> f2 {new Foo()};
+  Foo* f2_ptr = f2.get();
+  //f2.release();
+  std::cout << f2_ptr << '\n';
+  return f2_ptr;
+}
+
 int main(int argc, char** argv) {
+  /*
   std::cout << "Creating new Foo...\n";
   Foo* f;
   {
@@ -26,7 +39,7 @@ int main(int argc, char** argv) {
   std::list<Foo*> list;
   int x = 5;
   while (x >= 0) {
-    std::unique_ptr<Foo> f(new Foo());
+    std::unique_ptr<Foo> f{new Foo()};
     list.push_back(f.get());
     x--;
   }
@@ -34,5 +47,14 @@ int main(int argc, char** argv) {
     Foo* tmp = *it;
     tmp->doit();
   }
+
+  std::cout << "give up..." << '\n';
+  std::unique_ptr<Foo> give_up{new Foo()};
+  Foo* fp = give_up.release();
+  */
+
+  Foo* f = something();
+  f->doit();
+  
   return 0;
 }
